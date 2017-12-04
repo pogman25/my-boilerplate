b1e58da96316d16e8aabc786b15846bf870fbcd2
Alexandr Pogorelov <apogorelov@smedialink.com>
2017-12-04 08:14:53 +0300

    login page fix design

--- src/app/containers/Login/index.tsx
+++ src/app/containers/Login/index.tsx
@@ -33,17 +33,16 @@ class Login extends React.Component<ILogin, any> {
         } else {
             return(
                 <div className={styles.loginPage}>
-                    <div>
-                        <p>Мой Яндекс Диск</p>
-                    </div>
-                    <div>
-                        здесь будут картинки
-                    </div>
-                    <div>
-                        <a
-                            className={styles.authButton}
-                            href='https://oauth.yandex.ru/authorize?response_type=token&client_id=443eadf72ce14612a901a68f83588c90'
-                        >Авторизоваться</a>
+                    <div className={styles.mainPic}>
+                        <div>
+                            <p>Мой Яндекс Диск</p>
+                        </div>
+                        <div>
+                            <a
+                                className={styles.authButton}
+                                href='https://oauth.yandex.ru/authorize?response_type=token&client_id=443eadf72ce14612a901a68f83588c90'
+                            >Авторизоваться</a>
+                        </div>
                     </div>
                 </div>
             )
